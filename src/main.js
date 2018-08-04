99fd8a39247abc796cd31a0add7014acdf6c58d4
yy <1677150733@qq.com>
2018-08-04 08:17:41 +0800

    添加mint-ui

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import mintUI from 'mint-ui'
 
 Vue.config.productionTip = false
+Vue.use(mintUI)
 
 /* eslint-disable no-new */
 new Vue({
